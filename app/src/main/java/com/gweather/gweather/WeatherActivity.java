7d754ff3dc197363adfb9b54f778810662576726
cospi <860944375@qq.com>
2018-04-06 19:58:25 +0800

    增加后台自动更新天气功能。

--- app/src/main/java/com/gweather/gweather/WeatherActivity.java
+++ app/src/main/java/com/gweather/gweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.gweather.gweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.gweather.gweather.gson.Forecast;
 import com.gweather.gweather.gson.Weather;
+import com.gweather.gweather.service.AutoUpdateService;
 import com.gweather.gweather.util.HttpUtil;
 import com.gweather.gweather.util.Utility;
 
@@ -167,6 +169,9 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather", responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         } else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                         }
